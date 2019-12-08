1d4886a672bda0519ec05a8b22662668f5957967
Conver <conversun@gmail.com>
2019-12-09 01:06:25 +0800

    fix: update

--- Surge/JS/kela.js
+++ Surge/JS/kela.js
@@ -3,13 +3,17 @@ Gyroscope unlock pro (Script author: @Maasea )
 Surge:
 http-response ^https:\/\/api\.gyrosco\.pe\/v1\/account\/$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/gyroscope.js
 
-http-response ^https:\/\/claritywallpaper\.com\/clarity\/api\/userInfo requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/didez/Script/master/Surge/JS/kela.js
+http-response ^https:\/\/claritywallpaper\.com\/clarity\/api\/special\/* requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/didez/Script/master/Surge/JS/kela.js
 
 https://claritywallpaper.com/clarity/api/userInfo
-
+https://claritywallpaper.com/clarity/api/special/ff8080816eaba6b0016ece798d3756e5
 MITM = claritywallpaper.com
 */
 
 let obj = JSON.parse($response.body);
+obj.pictureList.forEach(item => {
+    
+});
+
 obj.data["isFree"] = true;
 $done({body: JSON.stringify(obj)});
